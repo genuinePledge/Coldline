f2d85ba68597b7bf43e033475e868cbd02416500
genuinePledge <uncmishu@gmail.com>
2021-02-15 00:54:39 +0900

    added mouse control of a player

--- Super2DGameYo/Game.cpp
+++ Super2DGameYo/Game.cpp
@@ -1,13 +1,16 @@
 #include "Game.h"
 #include "Player.h"
+#include "Utility.h"
+#include <math.h>
+#include <iostream>
 
-Player player(100.f, 100.f);
+Player player(100.f, 100.f, "res/tex/derebass.jpg");
 
 Game::Game(uint16_t sizeX, uint16_t sizeY, const std::string& name)
 {
-	window = new sf::RenderWindow(sf::VideoMode(sizeX, sizeY), name, sf::Style::Close);
-	player.setTex("res/tex/derebass.jpg");
-	player.setOrigin(100.f, 100.f);
+	window = new sf::RenderWindow(sf::VideoMode(sizeX, sizeY), name, sf::Style::Default);
+	player.setOrigin(50.f, 50.f);
+	player.setPosition(sizeX / 2, sizeY / 2);
 }
 
 Game::~Game()
@@ -49,8 +52,20 @@ void Game::update()
 		}
 	}
 
-	sf::Vector2i mousePos = sf::Mouse::getPosition(*window);
-	player.setPosition((float)mousePos.x, (float)mousePos.y);
+	float angle = utility::angle((sf::Vector2f)sf::Mouse::getPosition(*window) - player.getPosition(), sf::Vector2f(player.getPosition().x, player.getPosition().y - 20.f) - player.getPosition()) * 180.f / 3.141592653589f;
+	if (sf::Mouse::getPosition(*window).x < player.getPosition().x)
+		angle = 360.f - angle;
+	player.setRotation(angle);
+	std::cout << "mouse coord: (" << sf::Mouse::getPosition(*window).x << ", " << sf::Mouse::getPosition(*window).y << ")  angle: " << angle << " deg.\n";
+
+	if (sf::Keyboard::isKeyPressed(sf::Keyboard::A))
+		player.move(-1.f, 0.f);
+	if (sf::Keyboard::isKeyPressed(sf::Keyboard::D))
+		player.move( 1.f, 0.f);
+	if (sf::Keyboard::isKeyPressed(sf::Keyboard::W))
+		player.move(  0.f, -1.f);
+	if (sf::Keyboard::isKeyPressed(sf::Keyboard::S))
+		player.move( 0.f, 1.f);
 }
 
 void Game::render()
