4c71463057f6b4a0ca96afa7ca6460e7aed33fa7
genuinePledge <uncmishu@gmail.com>
2021-02-15 00:52:03 +0900

    added utility class for math

---
+++ Super2DGameYo/Utility.cpp
@@ -0,0 +1,39 @@
+#include "Utility.h"
+
+namespace utility
+{
+    sf::Vector2f operator-(const sf::Vector2f& v)
+    {
+        return sf::Vector2f(-v.x, -v.y);
+    }
+
+    sf::Vector2f operator+(const sf::Vector2f& v)
+    {
+        return sf::Vector2f(+v.x, +v.y);
+    }
+
+    sf::Vector2f operator-(const sf::Vector2f& v1, const sf::Vector2f& v2)
+    {
+        return sf::Vector2f(v1.x - v2.x, v1.y - v2.y);
+    }
+
+    sf::Vector2f operator+(const sf::Vector2f& v1, const sf::Vector2f& v2)
+    {
+        return sf::Vector2f(v1.x + v2.x, v1.y + v2.y);
+    }
+
+    float operator*(const sf::Vector2f& v1, const sf::Vector2f& v2)
+    {
+        return v1.x * v2.x + v1.y * v2.y;
+    }
+
+    float norm(const sf::Vector2f& v)
+    {
+        return sqrt(v.x * v.x + v.y * v.y);
+    }
+
+    float angle(const sf::Vector2f& v1, const sf::Vector2f& v2)
+    {
+        return acos((v1 * v2) / (norm(v1) * norm(v2)));
+    }
+}
---
+++ Super2DGameYo/Utility.h
@@ -0,0 +1,15 @@
+#pragma once
+#include <math.h>
+#include <SFML/Graphics.hpp>
+
+namespace utility
+{
+	sf::Vector2f operator-(const sf::Vector2f& v);
+	sf::Vector2f operator+(const sf::Vector2f& v);
+	sf::Vector2f operator+(const sf::Vector2f& v1, const sf::Vector2f& v2);
+	sf::Vector2f operator-(const sf::Vector2f& v1, const sf::Vector2f& v2);
+
+	float operator*(const sf::Vector2f& v1, const sf::Vector2f& v2);
+	float norm(const sf::Vector2f& v);
+	float angle(const sf::Vector2f& v1, const sf::Vector2f& v2);
+}
