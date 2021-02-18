16ccd5258337795f25ae2471ccef9b331ee4ddb0
genuinePledge <uncmishu@gmail.com>
2021-02-19 01:02:55 +0900

    added res for map

---
+++ Super2DGameYo/res/tilesets/map_tileset.tsx
@@ -0,0 +1,155 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.4" tiledversion="1.4.3" name="map_tileset" tilewidth="16" tileheight="16" tilecount="256" columns="16">
+ <image source="../tex/map_tiles.png" width="256" height="256"/>
+ <tile id="0">
+  <objectgroup draworder="index" id="2">
+   <object id="1" type="collision_wall" x="11" y="12" width="5" height="4"/>
+  </objectgroup>
+ </tile>
+ <tile id="1">
+  <objectgroup draworder="index" id="2">
+   <object id="1" type="collision_wall" x="0.00347222" y="12" width="15.9965" height="4"/>
+  </objectgroup>
+ </tile>
+ <tile id="2">
+  <objectgroup draworder="index" id="2">
+   <object id="1" type="collision_wall" x="0" y="12" width="5" height="4"/>
+  </objectgroup>
+ </tile>
+ <tile id="3">
+  <objectgroup draworder="index" id="2">
+   <object id="1" type="collision_wall" x="0" y="12" width="16" height="4"/>
+  </objectgroup>
+ </tile>
+ <tile id="4">
+  <objectgroup draworder="index" id="2">
+   <object id="1" type="collision_wall" x="0" y="12" width="16" height="4"/>
+  </objectgroup>
+ </tile>
+ <tile id="16">
+  <objectgroup draworder="index" id="2">
+   <object id="1" type="collision_wall" x="11" y="0" width="5" height="16"/>
+  </objectgroup>
+ </tile>
+ <tile id="17">
+  <objectgroup draworder="index" id="3">
+   <object id="2" type="collision_wall" x="0" y="0" width="16" height="16"/>
+  </objectgroup>
+ </tile>
+ <tile id="18">
+  <objectgroup draworder="index" id="2">
+   <object id="1" type="collision_wall" x="0" y="0" width="5" height="16"/>
+  </objectgroup>
+ </tile>
+ <tile id="19">
+  <objectgroup draworder="index" id="2">
+   <object id="1" type="collision_wall" x="0" y="0" width="16" height="16"/>
+  </objectgroup>
+ </tile>
+ <tile id="20">
+  <objectgroup draworder="index" id="2">
+   <object id="1" type="collision_wa" x="0" y="0" width="16" height="16"/>
+  </objectgroup>
+ </tile>
+ <tile id="32">
+  <objectgroup draworder="index" id="2">
+   <object id="1" type="collision_wall" x="11" y="0" width="5" height="16"/>
+  </objectgroup>
+ </tile>
+ <tile id="33">
+  <objectgroup draworder="index" id="2">
+   <object id="1" type="collision_wall" x="0" y="12" width="16" height="4"/>
+  </objectgroup>
+ </tile>
+ <tile id="34">
+  <objectgroup draworder="index" id="2">
+   <object id="1" type="collision_wall" x="0" y="0" width="5" height="16"/>
+  </objectgroup>
+ </tile>
+ <tile id="35">
+  <objectgroup draworder="index" id="3">
+   <object id="2" type="collision_wall" x="0" y="0">
+    <polygon points="0,0 0,16 16,16 15.9961,12.0039 4.99609,11.9961 5,0"/>
+   </object>
+  </objectgroup>
+ </tile>
+ <tile id="36">
+  <objectgroup draworder="index" id="2">
+   <object id="1" type="collision_wall" x="0" y="16">
+    <polygon points="0,0 16,0 16,-16 11.0039,-16 11.0056,-4 0.0078125,-3.99609"/>
+   </object>
+  </objectgroup>
+ </tile>
+ <tile id="48">
+  <objectgroup draworder="index" id="2">
+   <object id="1" type="collision_wall" x="11" y="0" width="5" height="16"/>
+  </objectgroup>
+ </tile>
+ <tile id="49">
+  <objectgroup draworder="index" id="2">
+   <object id="1" type="collision_wall" x="0" y="0" width="16" height="16"/>
+  </objectgroup>
+ </tile>
+ <tile id="50">
+  <objectgroup draworder="index" id="2">
+   <object id="1" type="collision_wall" x="0" y="0" width="5" height="16"/>
+  </objectgroup>
+ </tile>
+ <tile id="51">
+  <objectgroup draworder="index" id="2">
+   <object id="1" type="collision_wall" x="0" y="0" width="16" height="16"/>
+  </objectgroup>
+ </tile>
+ <tile id="52">
+  <objectgroup draworder="index" id="2">
+   <object id="1" type="collision_wall" x="0" y="0" width="16" height="16"/>
+  </objectgroup>
+ </tile>
+ <tile id="101">
+  <objectgroup draworder="index" id="2">
+   <object id="1" type="collision_wall" x="0" y="0" width="16" height="16"/>
+  </objectgroup>
+ </tile>
+ <tile id="102">
+  <objectgroup draworder="index" id="2">
+   <object id="1" type="collision_wall" x="0" y="0" width="16" height="16"/>
+  </objectgroup>
+ </tile>
+ <tile id="103">
+  <objectgroup draworder="index" id="2">
+   <object id="1" type="collision_wall" x="0" y="0" width="16" height="16"/>
+  </objectgroup>
+ </tile>
+ <tile id="117">
+  <objectgroup draworder="index" id="2">
+   <object id="1" type="collision_wall" x="0" y="0" width="16" height="16"/>
+  </objectgroup>
+ </tile>
+ <tile id="119">
+  <objectgroup draworder="index" id="3">
+   <object id="3" type="collision_wall" x="0" y="0" width="16" height="16"/>
+  </objectgroup>
+ </tile>
+ <tile id="130">
+  <objectgroup draworder="index" id="2">
+   <object id="1" name="hole_with_ladder" x="0.565217" y="3.78261" width="14.6522" height="9.13043">
+    <ellipse/>
+   </object>
+  </objectgroup>
+ </tile>
+ <tile id="133">
+  <objectgroup draworder="index" id="2">
+   <object id="1" type="collision_wall" x="0" y="0" width="16" height="16"/>
+  </objectgroup>
+ </tile>
+ <tile id="134">
+  <objectgroup draworder="index" id="2">
+   <object id="1" type="collision_wall" x="0" y="0" width="16" height="16"/>
+  </objectgroup>
+ </tile>
+ <tile id="135">
+  <objectgroup draworder="index" id="2">
+   <object id="1" type="collision_wall" x="0" y="0" width="16" height="16"/>
+  </objectgroup>
+ </tile>
+</tileset>
---
+++ Super2DGameYo/res/tilesets/objects_tileset.tsx
@@ -0,0 +1,53 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.4" tiledversion="1.4.3" name="objects_tileset" tilewidth="16" tileheight="16" tilecount="256" columns="16">
+ <image source="../tex/obj_tiles.png" width="256" height="256"/>
+ <tile id="49">
+  <objectgroup draworder="index" id="2">
+   <object id="1" name="skull" x="4.52174" y="6.08696" width="6.91304" height="7.52174"/>
+  </objectgroup>
+ </tile>
+ <tile id="101">
+  <objectgroup draworder="index" id="2">
+   <object id="1" type="one_crate" x="0" y="11" width="16" height="5"/>
+  </objectgroup>
+ </tile>
+ <tile id="102">
+  <objectgroup draworder="index" id="2">
+   <object id="1" type="two_crate" x="0" y="0" width="16" height="16"/>
+  </objectgroup>
+ </tile>
+ <tile id="117">
+  <objectgroup draworder="index" id="2">
+   <object id="1" type="one_crate" x="0" y="0" width="16" height="16"/>
+  </objectgroup>
+ </tile>
+ <tile id="118">
+  <objectgroup draworder="index" id="2">
+   <object id="1" type="two_crate" x="0" y="0" width="16" height="16"/>
+  </objectgroup>
+ </tile>
+ <tile id="152">
+  <animation>
+   <frame tileid="152" duration="190"/>
+   <frame tileid="153" duration="190"/>
+   <frame tileid="154" duration="190"/>
+   <frame tileid="155" duration="190"/>
+   <frame tileid="156" duration="190"/>
+   <frame tileid="157" duration="190"/>
+   <frame tileid="158" duration="190"/>
+   <frame tileid="159" duration="190"/>
+  </animation>
+ </tile>
+ <tile id="168">
+  <animation>
+   <frame tileid="168" duration="190"/>
+   <frame tileid="169" duration="190"/>
+   <frame tileid="170" duration="190"/>
+   <frame tileid="171" duration="190"/>
+   <frame tileid="172" duration="190"/>
+   <frame tileid="173" duration="190"/>
+   <frame tileid="174" duration="190"/>
+   <frame tileid="175" duration="190"/>
+  </animation>
+ </tile>
+</tileset>
