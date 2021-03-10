bc54666685f199eeda81bb81064cfa6193b09afa
genuinePledge <uncmishu@gmail.com>
2021-03-10 19:49:29 +0900

    overriden methods from object in collider

--- Super2DGameYo/Collider.cpp
+++ Super2DGameYo/Collider.cpp
@@ -3,3 +3,13 @@
 Collider::Collider(float x, float y, float width, float height)
 {
 }
+
+sf::RectangleShape Collider::getRekt() const
+{
+    return m_body;
+}
+
+void Collider::render(sf::RenderTarget& target) const
+{
+    target.draw(m_body);
+}
--- Super2DGameYo/Collider.h
+++ Super2DGameYo/Collider.h
@@ -7,6 +7,9 @@ class Collider : public Object
 public:
 	Collider(float x, float y, float width, float height);
 
+	sf::RectangleShape getRekt() const override;
+	void render(sf::RenderTarget& target) const override;
+
 private:
 	sf::RectangleShape m_body;
 };
