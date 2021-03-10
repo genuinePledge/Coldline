1d45ee1f719d85d42dbc00d7f8be0339ad6f4866
genuinePledge <uncmishu@gmail.com>
2021-03-10 20:40:54 +0900

    set up getters and setters in tileset class

--- Super2DGameYo/Tileset.h
+++ Super2DGameYo/Tileset.h
@@ -9,6 +9,15 @@ public:
 
 	std::vector<sf::IntRect> texCoords;
 
+	void		 setName(const std::string& name);
+	void		 setTilecount(size_t count);
+	void		 setColumnCount(size_t count);
+	void		 setTextureSize(sf::Vector2i);
+	std::string  getName()		  const;
+	size_t		 getTilecount()	  const;
+	size_t		 getColumnCount() const;
+	sf::Vector2i getTextureSize() const;
+
 private:
 	std::string m_name;
 	uint16_t m_tilecount;
