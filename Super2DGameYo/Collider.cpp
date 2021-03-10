968932e116d4201217870dcdaac649c1d953c457
genuinePledge <uncmishu@gmail.com>
2021-03-10 19:56:38 +0900

    updated collider constructor

--- Super2DGameYo/Collider.cpp
+++ Super2DGameYo/Collider.cpp
@@ -1,7 +1,12 @@
 #include "Collider.h"
 
 Collider::Collider(float x, float y, float width, float height)
+    : Object(x, y)
 {
+    m_color = sf::Color(0, 255, 0, 100); // color light green
+    m_body.setFillColor(m_color);
+    m_body.setPosition(x, y);
+    m_body.setSize(sf::Vector2f(width, height));
 }
 
 sf::RectangleShape Collider::getRekt() const
