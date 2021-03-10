b6c3c19fca9a541ff2ac4c6fa28e54fa15cb19cf
genuinePledge <uncmishu@gmail.com>
2021-03-10 23:53:58 +0900

    fix naming issues

--- Super2DGameYo/res/tilesets/objects_tileset.tsx
+++ Super2DGameYo/res/tilesets/objects_tileset.tsx
@@ -1,6 +1,6 @@
 <?xml version="1.0" encoding="UTF-8"?>
 <tileset version="1.4" tiledversion="1.4.3" name="objects_tileset" tilewidth="16" tileheight="16" tilecount="256" columns="16">
- <image source="../tex/obj_tiles.png" width="256" height="256"/>
+ <image source="../tex/objects_tileset.png" width="256" height="256"/>
  <tile id="49">
   <objectgroup draworder="index" id="2">
    <object id="1" name="skull" x="4.52174" y="6.08696" width="6.91304" height="7.52174"/>
