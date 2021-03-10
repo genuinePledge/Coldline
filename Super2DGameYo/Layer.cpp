f2926918eb47b3b3ced875f42991b7f568b7f4ad
genuinePledge <uncmishu@gmail.com>
2021-03-10 20:48:45 +0900

    added parse method in layer

--- Super2DGameYo/Layer.cpp
+++ Super2DGameYo/Layer.cpp
@@ -1,1 +1,32 @@
 #include "Layer.h"
+
+Layer::Layer()
+{
+}
+
+Layer::~Layer()
+{
+}
+
+void Layer::parse(const pugi::xml_node& node)
+{
+	pugi::xml_node custom_property = node.child("properties").first_child();
+
+	while (custom_property)
+	{
+		if (!std::string("tileset_id").compare(custom_property.attribute("name").value()))
+			m_tilesetID = atoi(custom_property.attribute("value").value());
+		custom_property = custom_property.next_sibling();
+	}
+
+	pugi::xml_node data = node.child("data");
+	std::stringstream ss(data.text().get());
+	std::string str;
+
+	m_name = node.attribute("name").value();
+	m_width = atoi(node.attribute("width").value());
+	m_height = atoi(node.attribute("height").value());
+
+	while (std::getline(ss, str, ','))
+		m_data.push_back(atoi(str.c_str()));
+}
--- Super2DGameYo/Layer.h
+++ Super2DGameYo/Layer.h
@@ -1,5 +1,8 @@
 #pragma once
 #include "SFML/Graphics.hpp"
+#include "Tileset.h"
+#include "pugi-xml/pugixml.hpp"
+#include <sstream>
 
 class Layer
 {
@@ -7,10 +10,18 @@ public:
 	Layer();
 	~Layer();
 
+	void parse(const pugi::xml_node& node);
 
 private:
 	sf::VertexArray m_vertices;
 	std::vector<uint16_t> m_data;
+	Tileset m_tileset;
+
+	std::string m_name;
+	uint16_t m_width;
+	uint16_t m_height;
+
+	uint8_t m_tilesetID;
 
 };
 
