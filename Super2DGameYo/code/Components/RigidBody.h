20376328409b9c8193252d46e21f5d10bddead91
genuinePledge <uncmishu@gmail.com>
2021-03-30 19:27:31 +0900

    modified rigid body to support smooth movement

--- Super2DGameYo/code/Components/RigidBody.h
+++ Super2DGameYo/code/Components/RigidBody.h
@@ -4,5 +4,8 @@
 struct RigidBody
 {
 	sf::Vector2f velocity;
+	float acceleration;
+	float deceleration;
 	float speed;
+	RigidBody() : velocity(sf::Vector2f(0, 0)), acceleration(0.f), deceleration(0.f), speed(0.f) {}
 };
