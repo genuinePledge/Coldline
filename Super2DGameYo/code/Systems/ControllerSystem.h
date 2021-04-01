59d199e1578d726d6645490d4256ed7d5d0e10d1
genuinePledge <uncmishu@gmail.com>
2021-04-02 02:28:19 +0900

    only player reacts to mouse movement now

--- Super2DGameYo/code/Systems/ControllerSystem.h
+++ Super2DGameYo/code/Systems/ControllerSystem.h
@@ -7,16 +7,24 @@ class ControllerSystem : public IUpdateSystem
 {
 	virtual void update(entt::registry& registry, float dt) override
 	{
-		registry.view<RigidBody, Controller>().each([&](auto entity, RigidBody& body)
+		registry.view<RigidBody, Controller, Transform>().each([&](auto entity, RigidBody& body, Transform& transform)
 		{
 			if (sf::Keyboard::isKeyPressed(sf::Keyboard::A))
-				body.velocity.x -= body.acceleration * dt;
+				body.velocity.x -= body.acceleration;
 			if (sf::Keyboard::isKeyPressed(sf::Keyboard::S))
-				body.velocity.y += body.acceleration * dt;
+				body.velocity.y += body.acceleration;
 			if (sf::Keyboard::isKeyPressed(sf::Keyboard::D))
-				body.velocity.x += body.acceleration * dt;
+				body.velocity.x += body.acceleration;
 			if (sf::Keyboard::isKeyPressed(sf::Keyboard::W))
-				body.velocity.y -= body.acceleration * dt;
+				body.velocity.y -= body.acceleration;
+
+			auto& win = Locator::MainWindow::ref().get();
+			sf::Vector2f mousePos = win.mapPixelToCoords(sf::Mouse::getPosition(win));
+			float angle = vect::angle(mousePos - transform.position, sf::Vector2f(transform.position.x, transform.position.y - 1.f) - transform.position);
+			if (mousePos.x < transform.position.x)
+				angle = 360.f - angle;
+
+			transform.rotation = angle;
 		});
 	}
 };
