93c281f3f300f5ac01bf65ae3fd1aa942b8dbf2b
genuinePledge <uncmishu@gmail.com>
2021-04-11 01:43:00 +0900

    added statemanager instance and moved function bodies to cpp file

---
+++ Super2DGameYo/code/Game.cpp
@@ -0,0 +1,71 @@
+#include "Game.h"
+
+#include "State/StateMainMenu.h"
+#include "Locator.h"
+
+void Game::init()
+{
+	Locator::MainWindow::set(false);
+	Locator::Registry::set();
+	m_stateManager.init<StateMainMenu>();
+}
+
+void Game::run()
+{
+	constexpr uint8_t TICKRATE = 120;
+	constexpr float MS_PER_UPDATE = 1000.f / (float)TICKRATE;
+	float m_lastFpsTime = 0.f;;
+	uint16_t m_fps = 0;
+	float lag = 0.f;
+
+	while (Locator::MainWindow::ref().get().isOpen() && !m_stateManager.isEmpty()) {
+		auto delta = m_clock.restart().asMilliseconds();
+
+		m_lastFpsTime += delta;
+		lag += delta;
+		m_fps++;
+
+		if (m_lastFpsTime >= 1000)
+		{
+			Locator::MainWindow::ref().get().setTitle(Locator::MainWindow::ref().title + std::to_string(m_fps));
+			m_fps = 0;
+			m_lastFpsTime = 0;
+		}
+
+		handleEvents();
+
+		if (lag >= MS_PER_UPDATE)
+		{
+			m_stateManager.update((float)delta);
+			lag -= MS_PER_UPDATE;
+		}
+
+		Locator::MainWindow::ref().get().clear();
+
+		m_stateManager.render();
+
+		Locator::MainWindow::ref().get().display();
+
+		m_stateManager.updateStates();
+	}
+}
+
+void Game::handleEvents()
+{
+	sf::Event e;
+	auto& window = Locator::MainWindow::ref().get();
+
+	while (window.pollEvent(e))
+	{
+		m_stateManager.handleEvents(e);
+		switch (e.type)
+		{
+		case sf::Event::Closed:
+			window.close();
+			break;
+
+		default:
+			break;
+		}
+	}
+}
--- Super2DGameYo/code/Game.h
+++ Super2DGameYo/code/Game.h
@@ -1,64 +1,18 @@
 #pragma once
 #include "SFML/Graphics.hpp"
-#include "entt/entt.hpp"
 #include "State/StateManager.h"
-#include "State/StateMainMenu.h"
-#include "Scene/MainMenu.h"
 
 class Game
 {
 public:
 	Game() = default;
 
-
-	void init()
-	{
-		Locator::MainWindow::set(false);
-		Locator::Registry::set();
-		stateManager.init<StateMainMenu>(std::make_unique<MainMenu>());
-	}
-
-	void run()
-	{
-		constexpr uint8_t TICKRATE = 120;
-		constexpr float MS_PER_UPDATE = 1000.f / (float)TICKRATE;
-		float m_lastFpsTime = 0.f;;
-		uint16_t m_fps = 0;
-		float lag = 0.f;
-
-		while (Locator::MainWindow::ref().get().isOpen() && !stateManager.isEmpty()) {
-			auto delta = m_clock.restart().asMilliseconds();
-
-			m_lastFpsTime += delta;
-			lag += delta;
-			m_fps++;
-
-			if (m_lastFpsTime >= 1000)
-			{
-				Locator::MainWindow::ref().get().setTitle(Locator::MainWindow::ref().title + std::to_string(m_fps));
-				m_fps = 0;
-				m_lastFpsTime = 0;
-			}
-
-			if (lag >= MS_PER_UPDATE)
-			{
-				stateManager.update((float)delta);
-				lag -= MS_PER_UPDATE;
-			}
-
-			Locator::MainWindow::ref().get().clear();
-
-			stateManager.render();
-
-			Locator::MainWindow::ref().get().display();
-
-			stateManager.handleEvents();
-			stateManager.updateStates();
-		}
-	}
+	void init();
+	void run();
+	void handleEvents();
 
 private:
 	sf::Clock m_clock;
 	
-	StateManager stateManager;
+	StateManager m_stateManager;
 };
