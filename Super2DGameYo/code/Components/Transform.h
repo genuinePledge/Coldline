ac3835fd6b4703a98e19e2e783a722e342c82270
genuinePledge <uncmishu@gmail.com>
2021-04-13 17:22:27 +0900

    added default values

--- Super2DGameYo/code/Components/Transform.h
+++ Super2DGameYo/code/Components/Transform.h
@@ -3,8 +3,8 @@
 
 struct Transform
 {
-	sf::Vector2f position;
-	sf::Vector2f scale;
-	sf::Vector2f origin;
-	float rotation;
+	sf::Vector2f position = sf::Vector2f(0.f, 0.f);
+	sf::Vector2f scale	  = sf::Vector2f(1.f, 1.f);
+	sf::Vector2f origin	  = sf::Vector2f(0.f, 0.f);
+	float rotation		  = 0.f;
 };
