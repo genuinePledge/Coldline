dea0ecd44af89d39dc87b48247da71993b60461f
genuinePledge <uncmishu@gmail.com>
2021-04-16 23:14:21 +0900

    added debug geometry rendering system

---
+++ Super2DGameYo/code/Components/Debug.h
@@ -0,0 +1,12 @@
+#pragma once
+#include "SFML/Graphics.hpp"
+
+struct DebugCircle
+{
+	sf::CircleShape circle;
+};
+
+struct DebugRect
+{
+	sf::RectangleShape rect;
+};
---
+++ Super2DGameYo/code/Systems/RenderDebugSystem.h
@@ -0,0 +1,27 @@
+#pragma once
+#include "IRenderSystem.h"
+#include "../Components/Debug.h"
+#include "../Components/RigidBody.h"
+#include "../Locator.h"
+
+class RenderDebugSystem : public IRenderSystem
+{
+	virtual void render(entt::registry& reg, sf::RenderTarget& target) override
+	{
+		{
+			reg.view<DebugRect, RigidBody>().each([&](auto entity, DebugRect& debug, RigidBody& phys)
+			{
+				debug.rect.setPosition(Locator::MainWindow::ref().worldToScreenPos(phys.body->GetPosition()));
+				target.draw(debug.rect);
+			});
+		}
+
+		{
+			reg.view<DebugCircle, RigidBody>().each([&](auto entity, DebugCircle& debug, RigidBody& phys)
+			{
+				debug.circle.setPosition(Locator::MainWindow::ref().worldToScreenPos(phys.body->GetPosition()));
+				target.draw(debug.circle);
+			});
+		}
+	}
+};
