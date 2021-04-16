0ecf2d5af7734d760859c8a3106ad698aa94e6a7
genuinePledge <uncmishu@gmail.com>
2021-04-16 23:17:38 +0900

    added changes to accomodate box2d

--- Super2DGameYo/code/State/StatePlaying.cpp
+++ Super2DGameYo/code/State/StatePlaying.cpp
@@ -3,12 +3,12 @@
 #include "StateManager.h"
 #include "StateMainMenu.h"
 
-#include "../Systems/CollisionHandlingSystem.h"
 #include "../Systems/ControllerSystem.h"
 #include "../Systems/MovementSystem.h"
-#include "../Systems/RenderColliderSystem.h"
 #include "../Systems/RenderMapSystem.h"
 #include "../Systems/RenderSpriteSystem.h"
+#include "../Systems/UpdatePhysicsSystem.h"
+#include "../Systems/RenderDebugSystem.h"
 
 #include "../Locator.h"
 
@@ -82,18 +82,20 @@ void StatePlaying::handleEvents(sf::Event e)
 void StatePlaying::initSystems()
 {
 	m_updateSystems.emplace_back(std::make_unique<ControllerSystem>());
-	m_updateSystems.emplace_back(std::make_unique<CollisionHandlingSystem>());
 	m_updateSystems.emplace_back(std::make_unique<MovementSystem>());
+	m_updateSystems.emplace_back(std::make_unique<UpdateSpriteSystem>());
+	m_updateSystems.emplace_back(std::make_unique<UpdatePhysicsSystem>());
 
 	m_renderSystems.emplace_back(std::make_unique<RenderMapSystem>());
 	m_renderSystems.emplace_back(std::make_unique<RenderSpriteSystem>());
-	m_renderSystems.emplace_back(std::make_unique<RenderColliderSystem>());
+	m_renderSystems.emplace_back(std::make_unique<RenderDebugSystem>());
 }
 
 void StatePlaying::setupEntities()
 {
 	auto& reg = Locator::Registry::ref();
 	auto& map = Locator::MainMap::ref();
+	auto& wnd = Locator::MainWindow::ref();
 
 	std::vector<std::shared_ptr<Object>> spawns(map.getObjects(Map::ObjType::spawns));
 	std::vector<std::shared_ptr<Object>> walls(map.getObjects(Map::ObjType::solids));
@@ -102,29 +104,37 @@ void StatePlaying::setupEntities()
 	for (auto i = 1u; i < spawns.size(); i++)
 	{
 		const auto entity = reg.create();
+
+		b2BodyDef bodyDef;
+		bodyDef.type = b2_dynamicBody;
+		bodyDef.fixedRotation = true;
+		bodyDef.position = wnd.screenToWorldPos(spawns[i]->getRekt().getPosition());
+
+		b2CircleShape shape;
+		shape.m_radius = wnd.getWorldSize({8.f, 8.f}).x;
+
+		b2FixtureDef fixtureDef;
+		fixtureDef.density = 1.f;
+		fixtureDef.friction = 0.f;
+		fixtureDef.restitution = 0.f;
+		fixtureDef.shape = &shape;
+
+		sf::CircleShape circle;
+		circle.setRadius(shape.m_radius);
+		circle.setOrigin({ 8.f, 8.f });
+		circle.setFillColor(sf::Color::Transparent);
+		circle.setOutlineColor(sf::Color::Red);
+		circle.setOutlineThickness(0.5f);
+
 		// ASSIGNING COMPONENTS
-		auto& transform = reg.emplace<Transform>(entity);
-		auto& material = reg.emplace<Material>(entity);
-		auto& sprite = reg.emplace<Sprite>(entity);
-		auto& rigidbody = reg.emplace<RigidBody>(entity);
-		auto& collider = reg.emplace<Collider>(entity,
-			transform.position.x,
-			transform.position.y,
-			16.f,
-			16.f);
+		auto& sprite = reg.emplace<sf::Sprite>(entity);
+		auto& rigidbody = reg.emplace<RigidBody>(entity, bodyDef, fixtureDef);
+		auto& debug = reg.emplace<DebugCircle>(entity, circle);
 
 		// SETTING UP COMPONENTS
-		transform.position = spawns[i]->getRekt().getPosition();
-		transform.origin = spawns[i]->getRekt().getOrigin();
 
-		material.texture = ResourceManager::get().m_texture.get("enemy");
-
-		sprite.vertices.setPrimitiveType(sf::Quads);
-		sprite.vertices.resize(4);
-		sprite.vertices[0] = sf::Vertex(sf::Vector2f(0.f, 0.f),								 sf::Vector2f(0.f, 0.f));
-		sprite.vertices[1] = sf::Vertex(sf::Vector2f(spawns[i]->getRekt().getSize().x, 0.f), sf::Vector2f(material.texture.getSize().x, 0.f));
-		sprite.vertices[2] = sf::Vertex(sf::Vector2f(spawns[i]->getRekt().getSize()),		 sf::Vector2f(material.texture.getSize()));
-		sprite.vertices[3] = sf::Vertex(sf::Vector2f(0.f, spawns[i]->getRekt().getSize().y), sf::Vector2f(0.f, material.texture.getSize().y));
+		sprite.setTexture(ResourceManager::get().m_texture.get("enemy"));
+		sprite.setOrigin(sprite.getLocalBounds().width / 2.f, sprite.getLocalBounds().height / 2.f);
 
 
 		// PUSHING TO THE CONTAINER OF ALL THE ENTITIES IN THE SCENE
@@ -141,25 +151,25 @@ void StatePlaying::setupEntities()
 
 	for (auto const& wall : walls)
 	{
+		b2BodyDef wallDef;
+		wallDef.type = b2_staticBody;
+		wallDef.position = { (wall->getRekt().getPosition().x + wall->getRekt().getOrigin().x) / Window::SCALING_FACTOR, (wall->getRekt().getPosition().y + wall->getRekt().getOrigin().y) / Window::SCALING_FACTOR };
+
+		b2PolygonShape shape;
+		shape.SetAsBox(wall->getRekt().getSize().x / 2 / Window::SCALING_FACTOR, wall->getRekt().getSize().y / 2 / Window::SCALING_FACTOR);
+
+		b2FixtureDef fixtureDef;
+		fixtureDef.shape = &shape;
+
+		sf::RectangleShape rect;
+		rect.setSize(wall->getRekt().getSize());
+		rect.setOrigin(wall->getRekt().getOrigin());
+		rect.setFillColor(sf::Color(0, 255, 0, 120));
+
 		const auto wall_entity = reg.create();
-		reg.emplace<Collider>(wall_entity, *dynamic_cast<Collider*>(wall.get()));
-		auto& sprite = reg.emplace<Sprite>(wall_entity);
-		auto& material = reg.emplace<Material>(wall_entity);
-		auto& transform = reg.emplace<Transform>(wall_entity);
+		auto& rigidBody = reg.emplace<RigidBody>(wall_entity, wallDef, fixtureDef);
+		auto& debug = reg.emplace<DebugRect>(wall_entity, rect);
 
-		material.color = sf::Color(0, 0, 255, 100);
-		material.texture = ResourceManager::get().m_texture.get("trans");
-
-		transform.position = wall->getRekt().getPosition();
-		transform.rotation = wall->getRekt().getRotation();
-		transform.scale = wall->getRekt().getScale();
-
-		sprite.vertices.setPrimitiveType(sf::Quads);
-		sprite.vertices.resize(4);
-		sprite.vertices[0] = sf::Vertex(sf::Vector2f(0.f, 0.f), sf::Vector2f(0.f, 0.f));
-		sprite.vertices[1] = sf::Vertex(sf::Vector2f(wall->getRekt().getSize().x, 0.f), sf::Vector2f(material.texture.getSize().x, 0.f));
-		sprite.vertices[2] = sf::Vertex(sf::Vector2f(wall->getRekt().getSize()), sf::Vector2f(material.texture.getSize()));
-		sprite.vertices[3] = sf::Vertex(sf::Vector2f(0.f, wall->getRekt().getSize().y), sf::Vector2f(0.f, material.texture.getSize().y));
 
 		m_entities.push_back(wall_entity);
 	}
@@ -167,44 +177,43 @@ void StatePlaying::setupEntities()
 	auto player = reg.create();
 	player = createPlayer(reg, player, spawns[0]->getRekt().getPosition(), sf::Vector2f(16.f, 16.f), "test_player_16x16");
 
-	auto& data = reg.emplace<CollisionData>(player);
-	data.colliders = walls;
-
 	m_entities.push_back(player);
 }
 
 entt::entity& StatePlaying::createPlayer(entt::registry& reg, entt::entity& player, sf::Vector2f pos, sf::Vector2f size, const std::string& texPath)
 {
-	auto& transform = reg.emplace<Transform>(player);
-	auto& material = reg.emplace<Material>(player);
-	auto& sprite = reg.emplace<Sprite>(player);
-	auto& body = reg.emplace<RigidBody>(player);
-	reg.emplace<Controller>(player);
-	auto& collider = reg.emplace<Collider>(player,
-		transform.position.x,
-		transform.position.y,
-		size.x,
-		size.y);
+	b2BodyDef bodyDef;
+	bodyDef.type = b2_dynamicBody;
+	bodyDef.position = Locator::MainWindow::ref().screenToWorldPos(pos);
+	bodyDef.fixedRotation = true;
+
+	b2CircleShape shape;
+	shape.m_radius = Locator::MainWindow::ref().getWorldSize(size / 2.f).x;
 	
-	transform.position = pos;
-	transform.origin = size / 2.f;
+	b2FixtureDef fixtureDef;
+	fixtureDef.density = 1.f;
+	fixtureDef.shape = &shape;
 
-	material.texture = ResourceManager::get().m_texture.get(texPath);
+	sf::CircleShape circle;
+	circle.setRadius(Window::SCALING_FACTOR * shape.m_radius);
+	circle.setOrigin(size.x / 2.f, size.y / 2.f);
+	circle.setFillColor(sf::Color::Transparent);
+	circle.setOutlineColor(sf::Color::Blue);
+	circle.setOutlineThickness(0.5f);
 
-	sprite.vertices.setPrimitiveType(sf::Quads);
-	sprite.vertices.resize(4);
-	sprite.vertices[0] = sf::Vertex(sf::Vector2f(0.f, 0.f), sf::Vector2f(0.f, 0.f));
-	sprite.vertices[1] = sf::Vertex(sf::Vector2f(size.x, 0.f), sf::Vector2f(material.texture.getSize().x, 0.f));
-	sprite.vertices[2] = sf::Vertex(sf::Vector2f(size), sf::Vector2f(material.texture.getSize()));
-	sprite.vertices[3] = sf::Vertex(sf::Vector2f(0.f, size.y), sf::Vector2f(0.f, material.texture.getSize().y));
+	auto& body = reg.emplace<RigidBody>(player, bodyDef, fixtureDef);
+	auto& sprite = reg.emplace<sf::Sprite>(player);
+	reg.emplace<Controller>(player);
+	auto& debug = reg.emplace<DebugCircle>(player, circle);
+	
+	sprite.setTexture(ResourceManager::get().m_texture.get(texPath));
+	sprite.setOrigin(sprite.getLocalBounds().width / 2.f, sprite.getLocalBounds().height / 2.f);
+	body.speed = 10.f;
 
-	body.speed = 2.2f;
-	body.acceleration = 0.3f;
-	body.deceleration = 0.2f;
 
 	auto& window = Locator::MainWindow::ref();
 	sf::View view;
-	view.setCenter(transform.position);
+	view.setCenter(pos);
 	view.setSize(static_cast<sf::Vector2f>(window.SCREEN_SIZE) / window.ZOOM_FACTOR);
 	window.setView(view);
 
