93e828414e182830a6da63d46f64790d0e8a4570
genuinePledge <uncmishu@gmail.com>
2021-04-20 21:17:02 +0900

    deleted collision info from tileset file

--- Super2DGameYo/res/tilesets/objects_tileset.tsx
+++ Super2DGameYo/res/tilesets/objects_tileset.tsx
@@ -1,31 +1,6 @@
-<?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.4" tiledversion="1.4.3" name="objects_tileset" tilewidth="16" tileheight="16" tilecount="256" columns="16">
+<	?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.5" tiledversion="1.5.0" name="objects_tileset" tilewidth="16" tileheight="16" tilecount="256" columns="16">
  <image source="../tex/objects_tileset.png" width="256" height="256"/>
- <tile id="49">
-  <objectgroup draworder="index" id="2">
-   <object id="1" name="skull" x="4.52174" y="6.08696" width="6.91304" height="7.52174"/>
-  </objectgroup>
- </tile>
- <tile id="101">
-  <objectgroup draworder="index" id="2">
-   <object id="1" type="one_crate" x="0" y="11" width="16" height="5"/>
-  </objectgroup>
- </tile>
- <tile id="102">
-  <objectgroup draworder="index" id="2">
-   <object id="1" type="two_crate" x="0" y="0" width="16" height="16"/>
-  </objectgroup>
- </tile>
- <tile id="117">
-  <objectgroup draworder="index" id="2">
-   <object id="1" type="one_crate" x="0" y="0" width="16" height="16"/>
-  </objectgroup>
- </tile>
- <tile id="118">
-  <objectgroup draworder="index" id="2">
-   <object id="1" type="two_crate" x="0" y="0" width="16" height="16"/>
-  </objectgroup>
- </tile>
  <tile id="152">
   <animation>
    <frame tileid="152" duration="190"/>
