e39c0a1d209d73d089a0fff4bd2d0d2fa6d093c5
genuinePledge <uncmishu@gmail.com>
2021-04-22 22:47:04 +0900

    new and neat debug rendering system

--- Super2DGameYo/code/Systems/RenderDebugSystem.h
+++ Super2DGameYo/code/Systems/RenderDebugSystem.h
@@ -1,6 +1,5 @@
 #pragma once
 #include "IRenderSystem.h"
-#include "../Components/Debug.h"
 #include "../Components/RigidBody.h"
 #include "../Locator.h"
 
@@ -9,20 +8,38 @@ class RenderDebugSystem : public IRenderSystem
 	virtual void render(entt::registry& reg, sf::RenderTarget& target) override
 	{
 		if (!Window::debugRender) return;
-		{
-			reg.view<DebugRect, RigidBody>().each([&](auto entity, DebugRect& debug, RigidBody& phys)
-			{
-				debug.rect.setPosition(Locator::MainWindow::ref().worldToScreenPos(phys.body->GetPosition()));
-				target.draw(debug.rect);
-			});
-		}
+		auto& wnd = Locator::MainWindow::ref();
 
+		reg.view<RigidBody>().each([&](auto entity, RigidBody& phys)
 		{
-			reg.view<DebugCircle, RigidBody>().each([&](auto entity, DebugCircle& debug, RigidBody& phys)
+			switch (phys.body->GetFixtureList()->GetShape()->m_type)
 			{
-				debug.circle.setPosition(Locator::MainWindow::ref().worldToScreenPos(phys.body->GetPosition()));
-				target.draw(debug.circle);
-			});
-		}
+			case b2Shape::Type::e_polygon:
+			{
+				sf::RectangleShape rect;
+				const auto body_size_world = static_cast<b2PolygonShape*>(phys.body->GetFixtureList()->GetShape())->m_vertices[2];
+				const auto body_size_screen = 2.f * wnd.getScreenSize(body_size_world);
+				rect.setSize(body_size_screen);
+				rect.setOrigin(rect.getSize() / 2.f);
+				rect.setPosition(wnd.worldToScreenPos(phys.body->GetPosition()));
+				rect.setFillColor(sf::Color(0, 255, 0, 120));
+				target.draw(rect);
+			}
+				break;
+			case b2Shape::Type::e_circle:
+			{
+				sf::CircleShape circle;
+				const auto radius = phys.body->GetFixtureList()->GetShape()->m_radius;
+				circle.setRadius(radius * wnd.SCALING_FACTOR);
+				circle.setOrigin(circle.getRadius(), circle.getRadius());
+				circle.setPosition(wnd.worldToScreenPos(phys.body->GetPosition()));
+				circle.setFillColor(sf::Color(0, 0, 255, 120));
+				target.draw(circle);
+			}
+				break;
+			default:
+				break;
+			}
+		});
 	}
 };
