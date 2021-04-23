2b6d0de62861abe882f76539cdb070e82ea49dcb
genuinePledge <uncmishu@gmail.com>
2021-04-24 00:04:22 +0900

    added dynamic view size change and speed mutability

--- Super2DGameYo/code/Systems/ControllerSystem.h
+++ Super2DGameYo/code/Systems/ControllerSystem.h
@@ -3,6 +3,7 @@
 #include "entt/entt.hpp"
 #include "../Components/ControllerComponent.h"
 #include "../Components/RigidBody.h"
+#include "../Locator.h"
 
 class ControllerSystem : public IUpdateSystem
 {
@@ -10,6 +11,7 @@ class ControllerSystem : public IUpdateSystem
 	{
 		registry.view<RigidBody, Controller, sf::Sprite>().each([&](auto entity, RigidBody& body, Controller& controller, sf::Sprite& sprite)
 		{
+			auto& win = Locator::MainWindow::ref();
 			controller.direction = { 0.f, 0.f };
 			if (sf::Keyboard::isKeyPressed(sf::Keyboard::A))
 				controller.direction.x = -1;
@@ -19,10 +21,27 @@ class ControllerSystem : public IUpdateSystem
 				controller.direction.x = 1;
 			if (sf::Keyboard::isKeyPressed(sf::Keyboard::W))
 				controller.direction.y = -1;
+			if (sf::Keyboard::isKeyPressed(sf::Keyboard::LShift))
+				controller.speed_type = Controller::SpeedType::fast;
+			else
+				controller.speed_type = Controller::SpeedType::normal;
 
-			auto& win = Locator::MainWindow::ref().get();
-			sf::Vector2i pixelPos = sf::Mouse::getPosition(win);
-			sf::Vector2f trueMousePos = win.mapPixelToCoords(pixelPos);
+
+			// TO-DO: GET RID OF THIS MESS BRUH
+			if (sf::Keyboard::isKeyPressed(sf::Keyboard::P))
+			{
+				win.ZOOM_FACTOR += 0.05;
+				win.getView().setSize(static_cast<sf::Vector2f>(win.SCREEN_SIZE) / win.ZOOM_FACTOR);
+			}
+			if (sf::Keyboard::isKeyPressed(sf::Keyboard::O))
+			{
+				win.ZOOM_FACTOR -= 0.05;
+				win.getView().setSize(static_cast<sf::Vector2f>(win.SCREEN_SIZE) / win.ZOOM_FACTOR);
+			}
+
+
+			sf::Vector2i pixelPos = sf::Mouse::getPosition(win.get());
+			sf::Vector2f trueMousePos = win.get().mapPixelToCoords(pixelPos);
 			float angle = vect::angle(trueMousePos - sprite.getPosition(), sf::Vector2f(sprite.getPosition().x, sprite.getPosition().y - 20.f) - sprite.getPosition());
 			if (trueMousePos.x < sprite.getPosition().x)
 				angle = 360.f - angle;
