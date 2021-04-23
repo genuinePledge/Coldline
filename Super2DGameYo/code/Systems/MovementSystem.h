4f645409ea92f26b87333148626d83fa47e7b5be
genuinePledge <uncmishu@gmail.com>
2021-04-24 00:04:46 +0900

    change speed according to speed type

--- Super2DGameYo/code/Systems/MovementSystem.h
+++ Super2DGameYo/code/Systems/MovementSystem.h
@@ -11,6 +11,21 @@ class MovementSystem : public IUpdateSystem
 		registry.view<RigidBody, Controller>().each([&](auto entity, RigidBody& body, Controller& controller)
 		{
 			auto& wnd = Locator::MainWindow::ref();
+
+			switch (controller.speed_type)
+			{
+			case Controller::SpeedType::fast:
+				body.speed = 16.f;
+				break;
+			
+			case Controller::SpeedType::normal:
+				body.speed = 10.f;
+				break;
+
+			default:
+				break;
+			}
+
 			b2Vec2 velocity = body.body->GetLinearVelocity();
 			b2Vec2 desiredVelocity{ 0.f, 0.f };
 			desiredVelocity.Set(controller.direction.x, controller.direction.y);
@@ -20,6 +35,7 @@ class MovementSystem : public IUpdateSystem
 			body.body->ApplyLinearImpulseToCenter(body.body->GetMass() * (desiredVelocity - velocity), true);
 			auto view = wnd.get().getView();
 			view.setCenter(wnd.worldToScreenPos(body.body->GetPosition()));
+			view.setSize(wnd.getView().getSize());
 			wnd.get().setView(view);
 			wnd.setView(view);
 		});
