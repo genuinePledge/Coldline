2277056daaf63a384917451d7762776339d6cc09
genuinePledge <uncmishu@gmail.com>
2021-05-06 17:42:50 +0900

    added win condition system

---
+++ Super2DGameYo/code/Systems/WinConditionSystem.h
@@ -0,0 +1,40 @@
+#pragma once
+#include "IUpdateSystem.h"
+#include "../Components/RigidBody.h"
+#include "../Components/WinCondition.h"
+#include "../Components/CollisionResponse.h"
+
+class WinConditionSystem : public IUpdateSystem
+{
+	virtual void update(entt::registry& reg, float dt) override
+	{
+		{
+			auto view = reg.view<RigidBody>();
+
+			for (auto entity : view)
+			{
+				if (reg.has<Controller>(entity))
+					continue;
+
+				auto&& [enemy] = view.get(entity);
+
+				if (enemy.body->GetFixtureList()->GetFilterData().categoryBits == static_cast<uint16>(CollisionMasks::ENEMY))
+					return;
+
+			}
+		}
+		
+
+		{
+			auto view = reg.view<WinCondition>();
+
+			for (auto entity : view)
+			{
+				auto&& [condition] = view.get(entity);
+
+				condition.success();
+			}
+		}
+
+	}
+};
