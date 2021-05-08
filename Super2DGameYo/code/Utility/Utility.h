c6ffe094c4d87f47a9861617a001e4759bd10d2d
genuinePledge <uncmishu@gmail.com>
2021-05-08 11:38:45 +0900

    deleted utility.cpp

--- Super2DGameYo/code/Utility/Utility.h
+++ Super2DGameYo/code/Utility/Utility.h
@@ -1,15 +1,51 @@
 #pragma once
 #include <SFML/Graphics.hpp>
+#include "Utility.h"
+#include <math.h>
+#include <string>
 
 namespace vect
 {
-	sf::Vector2f operator-(const sf::Vector2f& v);
-	sf::Vector2f operator+(const sf::Vector2f& v);
-	sf::Vector2f operator+(const sf::Vector2f& v1, const sf::Vector2f& v2);
-	sf::Vector2f operator-(const sf::Vector2f& v1, const sf::Vector2f& v2);
+    sf::Vector2f operator-(const sf::Vector2f& v)
+    {
+        return sf::Vector2f(-v.x, -v.y);
+    }
 
-	float operator*(const sf::Vector2f& v1, const sf::Vector2f& v2);
-	float norm(const sf::Vector2f& v);
-	float angle(const sf::Vector2f& v1, const sf::Vector2f& v2);
-	
+    sf::Vector2f operator+(const sf::Vector2f& v)
+    {
+        return sf::Vector2f(+v.x, +v.y);
+    }
+
+    sf::Vector2f operator-(const sf::Vector2f& v1, const sf::Vector2f& v2)
+    {
+        return sf::Vector2f(v1.x - v2.x, v1.y - v2.y);
+    }
+
+    sf::Vector2f operator+(const sf::Vector2f& v1, const sf::Vector2f& v2)
+    {
+        return sf::Vector2f(v1.x + v2.x, v1.y + v2.y);
+    }
+
+    float operator*(const sf::Vector2f& v1, const sf::Vector2f& v2)
+    {
+        return v1.x * v2.x + v1.y * v2.y;
+    }
+
+    float norm(const sf::Vector2f& v)
+    {
+        return sqrt(v.x * v.x + v.y * v.y);
+    }
+
+    float angle(const sf::Vector2f& v1, const sf::Vector2f& v2)
+    {
+        return (acos((v1 * v2) / (norm(v1) * norm(v2))) * 180.f / 3.141592653589f);
+    }
+
+    sf::Vector2f lerp(const sf::Vector2f& v1, const sf::Vector2f& v2, float t)
+    {
+        float x = std::lerp(v1.x, v2.x, t);
+        float y = std::lerp(v1.y, v2.y, t);
+
+        return { x, y };
+    }
 }
